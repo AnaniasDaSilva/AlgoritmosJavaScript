28b76cb667ec958112b0c25e332cd3481184efd7
AnaniasDaSilva <nanyhshs@gmail.com>
2022-09-18 16:44:35 -0300

    Acrescentando arquivos

---
+++ exercicio1.js
@@ -0,0 +1,12 @@
+var A = 2;
+var B = 3;
+var C = 5;
+
+var R = (A+B)**2
+console.log('valor de R é :' + R)
+var S = (B+C)**2
+console.log('valor de S é :' + S)
+var D = (R+S)/2 
+console.log('valor de D é :' + D)
+
+
---
+++ exercicio4.js
@@ -0,0 +1,10 @@
+var A = 7;
+var B = 3;
+
+var R = A%B;
+
+if (R != 1) {
+  console.log('é multiplo')
+} else {
+  console.log('não é multiplo')  
+}
